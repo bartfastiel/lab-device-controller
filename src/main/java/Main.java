cefbaf52b15f9af2661f87912bc1569e4c3ebcf3
bartfastiel <24777648+bartfastiel@users.noreply.github.com>
2025-12-22 20:53:55 +0100

    List serial com ports on console

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import com.fazecast.jSerialComm.*;
+
 import javax.swing.SwingUtilities;
 
 void main() {
+    var ports = SerialPort.getCommPorts();
+    if (ports.length == 0) {
+        System.out.println("No serial ports found.");
+        return;
+    }
+    Stream.of(ports).forEach(port ->
+        System.out.println("Found port: " + port.getSystemPortName() + " - " + port.getDescriptivePortName())
+    );
+
     SwingUtilities.invokeLater(MainWindow::new);
 }
